1771cfb06817186827df09e374d7963ae4b7b5c5
Rohit Vishwakarma <rohitvish29@gmail.com>
2020-04-16 11:52:21 -0400

    Random url generate

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const URL="https://cfw-takehome.developers.workers.dev/api/variants"
 async function handleRequest(request) {
 const apiResponse = await fetchResult()
 const finalresponse=await apiResponse.json()
-let selectedRandomUrl=fetchRandomVariant(finalresponse)
- return new Response(JSON.stringify(finalresponse), {
+let selectedRandomUrl=await fetchRandomVariant(finalresponse)
+ return new Response(JSON.stringify(selectedRandomUrl), {
     headers: { 'content-type': 'text/plain' },
   })
 }
@@ -24,10 +24,20 @@ const fetchResult = () => {
     return fetch(URL)
 }
 
-const fetchRandomVariant = (variants) => {
-  let a=Math.random(Math.random())
-  return fetch(variants[a])
+const fetchRandomVariant = async (array) => {
+  let a=Math.round(Math.random())
+  var obj = array;
+  console.log(a)
+  console.log((obj.variants[a]))
+      //  return (obj.variants[a]);
+    return await fetch(obj.variants[a]);
+
 }
+  
+
+
+  
+
 
 
 
